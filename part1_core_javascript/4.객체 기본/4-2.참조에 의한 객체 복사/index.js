3c11bc1496aa50cba01632b3c22301f6f60e58a5
parkchanwoo <pcw4214@gmail.com>
2021-08-09 23:57:49 +0900

    new 연산자 생성자 함수

--- part1_core_javascript/4.객체 기본/4-2.참조에 의한 객체 복사/index.js
+++ part1_core_javascript/4.객체 기본/4-2.참조에 의한 객체 복사/index.js
@@ -58,3 +58,20 @@ Object.assign(newUser, user);
 console.log(newUser);
 
 newUser.name = "park";
+
+//깊은 복사
+let user = {
+    name: "John",
+    sizes: {
+      height: 182,
+      width: 50
+    }
+  };
+  
+  let clone = Object.assign({}, user);
+  
+  alert( user.sizes === clone.sizes ); // true, 같은 객체입니다.
+  
+  // user와 clone는 sizes를 공유합니다.
+  user.sizes.width++;       // 한 객체에서 프로퍼티를 변경합니다.
+  alert(clone.sizes.width); // 51, 다른 객체에서 변경 사항을 확인할 수 있습니다.
